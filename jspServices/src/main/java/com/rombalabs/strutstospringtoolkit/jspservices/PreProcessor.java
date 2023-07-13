c09f6ab8a564cd9ddc37f3a9dbbef5345d1e40eb
Peter Romba <promba@gmail.com>
2023-07-13 11:12:50 -0500

    Remove unused import statements.

--- jspServices/src/main/java/com/rombalabs/strutstospringtoolkit/jspservices/PreProcessor.java
+++ jspServices/src/main/java/com/rombalabs/strutstospringtoolkit/jspservices/PreProcessor.java
@@ -10,8 +10,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.*;
-import java.nio.file.CopyOption;
-import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 
--- jspServices/src/main/java/com/rombalabs/strutstospringtoolkit/jspservices/transformers/PreprocessTransformer.java
+++ jspServices/src/main/java/com/rombalabs/strutstospringtoolkit/jspservices/transformers/PreprocessTransformer.java
@@ -1,7 +1,5 @@
 package com.rombalabs.strutstospringtoolkit.jspservices.transformers;
 
-import org.jsoup.nodes.Element;
-
 public interface PreprocessTransformer {
     String processText(String inputText);
 }
